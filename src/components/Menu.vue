7abddddd8fc16009add79d3c0f6083f347df4c63
宋金涛 <691736657@qq.com>
2023-03-10 11:24:53 +0800

    菜单改为由symbol确定是否选中并且保证刷新当前选中不丢失

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -2,8 +2,8 @@
   <div class="menu">
    <div class="_menu">
      <div class="menu_box">
-            <span :class="{active:index===current}" @click="dealClick(item,index)" v-for="(item,index) in list" :key="index">{{ item.classificationName}}
-            <img v-if="current === index" src="~@/assets/images/line.png" alt="">
+            <span :class="{active:item.symbol === current}" @click="dealClick(item,index)" v-for="(item,index) in list" :key="index">{{ item.classificationName}}
+            <img v-if="item.symbol === current" src="~@/assets/images/line.png" alt="">
         </span>
      </div>
      <div @click="onSearch" class="search_icon"></div>
@@ -17,36 +17,51 @@ export default {
   data(){
     return{
         list:[],
-        current:0
+        current:''
     }
   },
   created(){
+    let _symbol = sessionStorage.getItem("_symbol") || '';
     this.API.getClass().then(res=>{
         if(res.data.content.length > 0 ){
             this.list =  res.data.content;
-            this.$emit("check",res.data.content[0])
+            if(_symbol){
+              this.current = _symbol;
+              let tmp  = {};
+              res.data.content.forEach(item=>{
+                if(_symbol === item.symbol){
+                  tmp = item;
+                }
+              })
+              this.$emit("check",tmp)
+            }else{
+              let tmp = res.data.content[0];
+              this.current = tmp.symbol;
+              sessionStorage.setItem("_symbol",tmp.symbol)
+              this.$emit("check",tmp)
+            }
         }
     })
 
     // this.list = [
-    //     {symbol:111,classificationName:'首页'},
+    //     {symbol:11,classificationName:'首页'},
     //     {symbol:22,classificationName:'电影'},
     //     {symbol:111,classificationName:'电视剧'},
-    //     {symbol:111,classificationName:'动漫'},
-    //     {symbol:111,classificationName:'韩剧'},
-    //     {symbol:111,classificationName:'美剧'},
+    //     {symbol:1131,classificationName:'动漫'},
+    //     {symbol:11221,classificationName:'韩剧'},
+    //     {symbol:11771,classificationName:'美剧'},
     // ]
   },
   methods:{
-    dealClick(item,index){
-        this.current = index;
-        this.$emit('check',item);
+    dealClick(item){
+      this.current = item.symbol;
+      sessionStorage.setItem("_symbol",item.symbol)
+      this.$emit('check',item);
     },
     onSearch(){
       // let current = document.documentElement.classList.value;
       // document.documentElement.classList.remove(current);
       // document.documentElement.classList.add(current === 'dart' ? 'light' : 'dart');
-
       this.$router.push("/search");
     },
   }
